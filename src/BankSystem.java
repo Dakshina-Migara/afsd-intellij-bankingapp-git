333d10383081b3e0b33e5b9b0e00b70b09c9b6e0
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-10-17 14:36:18 +0530

    creating the arrays

---
+++ src/BankSystem.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BankSystem {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //creating the arrays
+        String[] userName = new String[50];
+        int[] accountNumber = new int[50];
+        double[] accountOpeningBalance = new double[50];
+        String[] accountType = new String[50];
+        double[] loanAmount = new double[50];
+        String[] loanDescription = new String[50];
+        int accountCount = 0;
+    }
+}
