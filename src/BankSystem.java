a84003b5acc6c24f7ee548cda87d17d4da470c49
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-10-17 14:40:06 +0530

    option menu

--- src/BankSystem.java
+++ src/BankSystem.java
@@ -12,5 +12,20 @@ public class BankSystem {
         double[] loanAmount = new double[50];
         String[] loanDescription = new String[50];
         int accountCount = 0;
+
+        while (true) {
+            System.out.println("===== WELCOME =====");
+            System.out.println("1. Create Account");
+            System.out.println("2. Deposit Money");
+            System.out.println("3. Withdraw Money");
+            System.out.println("4. Apply for a Loan");
+            System.out.println("5. Loan Details");
+            System.out.println("6. Fund Transfer");
+            System.out.println("7. Check Balance");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice (1–8): ");
+            int choice = scan.nextInt();
+            
+        }
     }
 }
